82da600a6dea388db2efa7cffb2ee34f0823bb3a
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-11-26 10:08:37 +0100

    Remove unused packages from flocke

--- hosts/flocke/packages.nix
+++ hosts/flocke/packages.nix
@@ -3,26 +3,17 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    # inputs.vivado2019flake.vivado-2019_2
-    mars-mips
-
-    obsidian
-
     # security audits
     lynis
-    jetbrains.idea-community
-    jetbrains.rust-rover
     baobab
     amdvlk
-    texlive.combined.scheme-full
-    android-tools
+    texlive.combined.scheme-medium
     signal-desktop
     nextcloud-client
     # etcher
     vlc
     audacity
     thunderbird
-    eclipses.eclipse-java
     openjdk
     # pkgs.nordvpn # nur.repos.LuisChDev.nordvpn
     material-icons
