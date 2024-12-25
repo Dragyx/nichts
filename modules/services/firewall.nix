ef3af3a664dccbce06cbf361730f3d9a1219eeae
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-12-25 22:03:34 +0100

    Remove unsafe firewall settings

--- modules/services/firewall.nix
+++ modules/services/firewall.nix
@@ -1,33 +1,5 @@
 {pkgs, ...}: {
-  # TODO: Move this under system options
-  # TODO: Make option to enable / disable restrictive port ranges
   networking.firewall = {
     enable = pkgs.lib.mkDefault true;
-    allowedTCPPorts = [
-      80
-      443
-      9418 # git
-      1194 # nordpnv
-      1716 # KDE Connect
-
-      # 22 # git
-    ];
-    allowedTCPPortRanges = [
-      {
-        from = 1739;
-        to = 1741;
-      } # KDE Connect
-    ];
-
-    allowedUDPPorts = [
-      1716 # KDE CONNECT
-    ];
-    allowedUDPPortRanges = [
-      # Some of these ports get assigned to discord...
-      {
-        from = 45000;
-        to = 60000;
-      }
-    ];
   };
 }
