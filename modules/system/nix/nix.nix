5b3c44099b4cd5ad2c56db19cacbfffaa575e697
Dragyx <66752602+Dragyx@users.noreply.github.com>
2025-03-05 16:58:22 +0100

    Remove helix substituter and trusted public keys, as the flake is not used any more

--- modules/system/nix/nix.nix
+++ modules/system/nix/nix.nix
@@ -16,13 +16,7 @@
         "nix-command" # experimental nix commands
         "cgroups" # allow nix to execute builds inside cgroups
       ];
-      substituters = [
-        "https://helix.cachix.org" # a cache for helix
-      ];
       warn-dirty = false;
-      trusted-public-keys = [
-        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-      ];
     };
   };
 }
