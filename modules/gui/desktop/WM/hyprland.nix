2a972a2acbc874deda990695eaf21cb072c3006e
Dragyx <66752602+Dragyx@users.noreply.github.com>
2025-03-21 20:27:26 +0100

    Remove failing service for polkit-gnome-authentication-agent

--- modules/gui/desktop/WM/hyprland.nix
+++ modules/gui/desktop/WM/hyprland.nix
@@ -42,19 +42,6 @@ in {
     security.pam.services.login.enableGnomeKeyring = cfg.gnome-keyring.enable;
 
     services.displayManager.sddm.wayland.enable = true;
-    systemd.user.services.polkit-gnome-authentication-agent-1 = mkIf cfg.gnome-keyring.enable {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = ["graphical-session.target"];
-      wants = ["graphical-session.target"];
-      after = ["graphical-session.target"];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
 
     home-manager.users.${username} = {
       home.packages = with pkgs; [
