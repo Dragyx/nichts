3f8e6ba8dc5f450d8230ffa19d4070615a0adf8a
Dragyx <66752602+Dragyx@users.noreply.github.com>
2025-08-14 17:13:37 +0200

    add jj support to starship

--- modules/cli/starship.nix
+++ modules/cli/starship.nix
@@ -15,6 +15,69 @@ in {
         enable = true;
         enableZshIntegration = config.modules.programs.zsh.enable;
         settings = {
+          git_status.disabled = true; # makes everything slow when jj is in use
+          git_commit.disabled = true;
+          git_metrics.disabled = true;
+          git_branch.disabled = true;
+          format = ''
+            $all''${custom.git_branch}''${custom.git_commit}''${custom.git_metrics}''${custom.git_status}
+          '';
+          # taken from https://github.com/jj-vcs/jj/wiki/Starship
+          custom = {
+            # custom module for jj status
+            # note that you'll need to add ${custom.git_branch}, ${custom.git_commit} etc
+            # into format: https://starship.rs/config/#default-prompt-format
+            jj = {
+              description = "The current jj status";
+              when = "jj --ignore-working-copy root";
+              symbol = "🥋 ";
+              command = ''
+                jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
+                  separate(" ",
+                    change_id.shortest(4),
+                    bookmarks,
+                    "|",
+                    concat(
+                      if(conflict, "💥"),
+                      if(divergent, "🚧"),
+                      if(hidden, "👻"),
+                      if(immutable, "🔒"),
+                    ),
+                    raw_escape_sequence("\x1b[1;32m") ++ if(empty, "(empty)"),
+                    raw_escape_sequence("\x1b[1;32m") ++ coalesce(
+                      truncate_end(29, description.first_line(), "…"),
+                      "∅",
+                    ) ++ raw_escape_sequence("\x1b[0m"),
+                  )
+                '
+              '';
+            };
+
+            git_status = {
+              when = "! jj --ignore-working-copy root";
+              command = "starship module git_status";
+              style = ""; # This disables the default "(bold green)" style
+              description = "Only show git_status if we're not in a jj repo";
+            };
+            git_commit = {
+              when = "! jj --ignore-working-copy root";
+              command = "starship module git_commit";
+              style = "";
+              description = "Only show git_commit if we're not in a jj repo";
+            };
+            git_metrics = {
+              when = "! jj --ignore-working-copy root";
+              command = "starship module git_metrics";
+              description = "Only show git_metrics if we're not in a jj repo";
+              style = "";
+            };
+            git_branch = {
+              when = "! jj --ignore-working-copy root";
+              command = "starship module git_branch";
+              description = "Only show git_branch if we're not in a jj repo";
+              style = "";
+            };
+          };
           add_newline = false;
           command_timeout = 1000;
           line_break = {
