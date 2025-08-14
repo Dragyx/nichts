33ec415c1a16e08c83fb881865b95b717f9c23ed
Dragyx <66752602+Dragyx@users.noreply.github.com>
2025-08-14 13:36:50 +0200

    Add kdeconnect firewall rules

--- modules/services/firewall.nix
+++ modules/services/firewall.nix
@@ -1,5 +1,19 @@
 {pkgs, ...}: {
   networking.firewall = {
     enable = pkgs.lib.mkDefault true;
+    allowedUDPPortRanges = [
+      {
+        # kdeconnect
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedTCPPortRanges = [
+      {
+        # kdeconnect
+        from = 1714;
+        to = 1764;
+      }
+    ];
   };
 }
