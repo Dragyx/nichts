3391f822c226cb68cc358eb58a91d97e1d60878d
Dragyx <66752602+Dragyx@users.noreply.github.com>
2025-09-29 18:06:54 +0800

    add jujutsu pager settings and alias

--- modules/cli/jujutsu.nix
+++ modules/cli/jujutsu.nix
@@ -6,7 +6,7 @@
 }: let
   username = config.modules.system.username;
   cfg = config.modules.programs.jujutsu;
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkEnableOption mkIf getExe;
 in {
   options.modules.programs.jujutsu.enable = mkEnableOption "jujutsu";
   config = mkIf cfg.enable {
@@ -14,7 +14,32 @@ in {
       programs.jujutsu = {
         enable = true;
         settings = {
-          ui.default-command = "log";
+          ui = {
+            default-command = "log";
+            pager = "${getExe pkgs.delta}";
+          };
+          "--scope" = [
+            {
+              "--when"."commands" = [
+                "status"
+                "log"
+              ];
+              ui = {
+                paginate = "never";
+              };
+            }
+          ];
+          aliases = {
+            # Inspired by https://copeberg.org/faukah/nichts/src/branch/main/modules/programs/cli/jujutsu.mod.nix
+            tug = [
+              "bookmark"
+              "move"
+              "-f"
+              "heads(::@- & bookmarks())"
+              "-t"
+              "@-"
+            ];
+          };
         };
       };
     };
