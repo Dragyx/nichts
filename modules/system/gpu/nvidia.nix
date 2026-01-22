9107daead6bccce194b2743a6950ec408a3ae0b6
Dragyx <66752602+Dragyx@users.noreply.github.com>
2026-01-22 13:18:31 +0100

    update nvidia settings

--- modules/system/gpu/nvidia.nix
+++ modules/system/gpu/nvidia.nix
@@ -5,7 +5,8 @@
   ...
 }: let
   cfg = config.modules.system.nvidia;
-  inherit (lib) mkIf mkEnableOption;
+  inherit (lib) mkIf mkEnableOption mkMerge;
+  nvidia-pkg = config.boot.kernelPackages.nvidiaPackages.stable;
 in {
   options.modules.system.nvidia.enable = mkEnableOption "nvidia";
   config = mkIf cfg.enable {
@@ -14,37 +15,55 @@ in {
     hardware.graphics = {
       enable = true;
       enable32Bit = true;
+      extraPackages = with pkgs; [
+        mesa
+        nvidia-vaapi-driver
+        libvdpau-va-gl
+        libva-vdpau-driver
+        egl-wayland
+      ];
     };
 
-    environment.systemPackages = [
-      pkgs.egl-wayland
+    environment.systemPackages = with pkgs; [
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-tools
+      libva
+      libva-utils
     ];
     hardware.nvidia = {
       modesetting.enable = true;
       open = false;
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = nvidia-pkg;
       powerManagement.enable = true;
       powerManagement.finegrained = false;
       nvidiaSettings = false;
     };
 
-    environment.sessionVariables = lib.mkIf config.modules.WM.hyprland.enable {
-      LIBVA_DRIVER_NAME = "nvidia";
-      XDG_SESSION_TYPE = "wayland";
-      GBM_BACKEND = "nvidia-drm";
-      GDK_BACKEND = "wayland";
-      # QT_QPA_PLATFORM = "wayland";
-      NIXOS_XDG_OPEN_USE_PORTAL = "1";
-      ELECTRON_OZONE_PLATFORM_HINT = "auto";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      __GL_GSYNC_ALLOWED = "0";
-      NIXOS_OZONE_WL = "1";
-      MOZ_ENABLE_WAYLAND = "1";
-      SDL_VIDEODRIVER = "wayland";
-      CLUTTER_BACKEND = "wayland";
-      XDG_CURRENT_DESKTOP = "Hyprland";
-      XDG_SESSION_DESKTOP = "Hyprland";
-      GTK_USE_PORTAL = "1";
-    };
+    environment.sessionVariables = mkMerge [
+      {
+        LIBVA_DRIVER_NAME = "nvidia";
+        GBM_BACKEND = "nvidia-drm";
+        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+        __GL_GSYNC_ALLOWED = "0";
+        __EGL_VENDOR_LIBRARY_FILENAMES = "${nvidia-pkg}/share/glvnd/egl_vendor.d/10_nvidia.json";
+      }
+      (mkIf config.modules.system.wayland {
+        XDG_SESSION_TYPE = "wayland";
+        GDK_BACKEND = "wayland";
+        QT_QPA_PLATFORM = "wayland";
+        NIXOS_XDG_OPEN_USE_PORTAL = "1";
+        ELECTRON_OZONE_PLATFORM_HINT = "auto";
+        NIXOS_OZONE_WL = "1";
+        MOZ_ENABLE_WAYLAND = "1";
+        SDL_VIDEODRIVER = "wayland";
+        CLUTTER_BACKEND = "wayland";
+        GTK_USE_PORTAL = "1";
+      })
+      (mkIf config.modules.WM.hyprland.enable {
+        XDG_CURRENT_DESKTOP = "Hyprland";
+        XDG_SESSION_DESKTOP = "Hyprland";
+      })
+    ];
   };
 }
