bde0f9800d7aa470bf62b09d0877e5e16768d90e
Dragyx <66752602+Dragyx@users.noreply.github.com>
2026-04-22 17:15:31 +0200

    Select correct foot theme

--- modules/theming/foot.nix
+++ modules/theming/foot.nix
@@ -7,36 +7,35 @@
 }:
 let
   polarity = if scheme.variant == "light" then "light" else "dark";
-  settings =
-    {
-      "colors-${polarity}" = with scheme; {
-        foreground = base05;
-        background = base00;
-        regular0 = base00;
-        regular1 = base08;
-        regular2 = base0B;
-        regular3 = base0A;
-        regular4 = base0D;
-        regular5 = base0E;
-        regular6 = base0C;
-        regular7 = base05;
-        bright0 = base03;
-        bright1 = base08;
-        bright2 = base0B;
-        bright3 = base0A;
-        bright4 = base0D;
-        bright5 = base0E;
-        bright6 = base0C;
-        bright7 = base07;
-        "16" = base09;
-        "17" = base0F;
-        "18" = base01;
-        "19" = base02;
-        "20" = base04;
-        "21" = base06;
-      };
-    }
-    |> mkIf enable;
+  settings = mkIf enable {
+    main.initial-color-theme = polarity;
+    "colors-${polarity}" = with scheme; {
+      foreground = base05;
+      background = base00;
+      regular0 = base00;
+      regular1 = base08;
+      regular2 = base0B;
+      regular3 = base0A;
+      regular4 = base0D;
+      regular5 = base0E;
+      regular6 = base0C;
+      regular7 = base05;
+      bright0 = base03;
+      bright1 = base08;
+      bright2 = base0B;
+      bright3 = base0A;
+      bright4 = base0D;
+      bright5 = base0E;
+      bright6 = base0C;
+      bright7 = base07;
+      "16" = base09;
+      "17" = base0F;
+      "18" = base01;
+      "19" = base02;
+      "20" = base04;
+      "21" = base06;
+    };
+  };
 in
 {
   # inspired by https://github.com/nix-community/stylix/blob/master/modules/foot/hm.nix
